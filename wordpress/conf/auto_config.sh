1e42354e325b9ff823ac3276dbd7eaa7b467b500
polodu13160 <paul.depetrini@gmail.com>
2026-06-18 22:01:29 +0200

    clean code

--- wordpress/conf/auto_config.sh
+++ wordpress/conf/auto_config.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
-# Petite pause pour laisser le temps à MariaDB de s'allumer
-sleep 100
-# 1. On entre dans le dossier monté par le volume
 cd /var/www/wordpress
 
-# 2. On vérifie si WordPress est déjà installé (pour éviter de tout casser si on redémarre le conteneur)
 if [ ! -f wp-config.php ]; then
-    echo "Téléchargement et configuration de WordPress..."
-    
-    # On télécharge WP avec l'outil officiel (plus propre que curl/tar)
+    echo "Creation WordPress"
     wp core download --allow-root --locale=fr_FR
-    
-    # Création du lien avec la base de données
     wp config create --allow-root \
         --dbname=$SQL_DATABASE \
         --dbuser=$SQL_USER \
         --dbpass=$SQL_PASSWORD \
         --dbhost=mariadb:3306
 
-    # Installation du site
     wp core install --allow-root \
         --url=pde-petr.42.fr \
         --title="Inception de Paul" \
@@ -27,7 +18,6 @@ if [ ! -f wp-config.php ]; then
         --admin_password=$WP_ADMIN_PASSWORD \
         --admin_email=$WP_ADMIN_EMAIL
 
-    # Création du deuxième utilisateur (Auteur)
     wp user create --allow-root \
         $WP_USER_LOGIN \
         $WP_USER_EMAIL \
@@ -35,8 +25,7 @@ if [ ! -f wp-config.php ]; then
         --user_pass=$WP_USER_PASSWORD
 fi
 
-# 3. On s'assure que les droits sont parfaits pour le serveur web !
-chown -R www-data:www-data /var/www/wordpress
+chown -R ${MY_UID}:www-data /var/www/wordpress
+chmod -R 775 /var/www/wordpress
 
-# 4. On lance la BONNE version de PHP au premier plan
 exec /usr/sbin/php-fpm8.2 -F
